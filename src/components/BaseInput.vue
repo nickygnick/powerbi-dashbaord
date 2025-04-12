d7a1970a8b85e6c3a5d61db0417caf25bf06a03f
MaxChou1205 <v2asault@yahoo.com.tw>
2025-04-12 22:21:04 +0800

    基礎 input 元件

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="mb-3" v-if="title">
+      {{ title }}
+    </div>
+    <input
+      class="h-11 w-full rounded-[4px] border-1 p-3 font-normal"
+      v-model="modelValue"
+      :class="[
+        type === 'secondary' && 'border-gray-400',
+        warning && 'border-red-waring',
+      ]"
+      type="text"
+      :placeholder="placeholder"
+      :disabled="disabled"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const modelValue = defineModel<string>();
+
+const {
+  title,
+  disabled,
+  placeholder,
+  type = "primary",
+  warning = false,
+} = defineProps<{
+  title?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  type?: "primary" | "secondary";
+  warning?: boolean;
+}>();
+</script>
+
+<style scoped></style>
