b3b2b8716e0b01d6855d512c45aaa2c1cb61619b
nickygnick <vm_sand0z>
2025-04-15 18:46:35 +0800

    config更新
    修改powerBI內簽網址

--- src/composables/sideMenuItems.js
+++ src/composables/sideMenuItems.js
@@ -3,114 +3,95 @@ import { ref } from "vue";
 export const useSideMenuItems = () => {
   const sideMenuItems = ref([
     {
-      id: 0,
-      title: "首頁",
-      parent: "首頁",
-      roleList: [
-        "售服主管",
-        "服務廠主管",
-        "接待主管",
-        "服務顧問",
-        "零件主管",
-        "零件人員",
-        "技術主管",
-        "技師",
-        "財務",
-        "保險專員",
-        "索賠員",
-      ],
-      link: "https://app.powerbi.com/reportEmbed?reportId=ed728c4b-7e05-44d9-92a0-c6cfc0e11838&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
-    },
-    {
       id: 1,
       title: "會計相關-應收帳款明細表",
       parent: "會計相關",
       roleList: ["售服主管", "服務廠主管", "財務"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=52986780cd9db57b5cdb&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=52986780cd9db57b5cdb"
     },
     {
       id: 2,
       title: "會計相關-發票明細表",
       parent: "會計相關",
       roleList: ["售服主管", "服務廠主管", "財務"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=1269a28a98bf9329c99f&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=1269a28a98bf9329c99f"
     },
     {
       id: 3,
       title: "經營相關-回廠率查詢",
       parent: "經營相關",
       roleList: ["售服主管", "服務廠主管"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=465a4f99554863911ac1&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=465a4f99554863911ac1"
     },
     {
       id: 4,
       title: "零件相關-出貨成本查詢",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=30a8c840a0ee740e9a3e&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=30a8c840a0ee740e9a3e"
     },
     {
       id: 5,
       title: "零件相關-零件進貨退回明細表",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=2be26881e08ac77ba1c3&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=2be26881e08ac77ba1c3"
     },
     {
       id: 6,
       title: "零件相關-零件銷售退回明細表",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=a0cc17df5274b4957aeb&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=a0cc17df5274b4957aeb"
     },
     {
       id: 7,
       title: "零件相關-零件出貨明細表",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=0f773a5e2ed0aa31816d&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=0f773a5e2ed0aa31816d"
     },
     {
       id: 8,
       title: "零件相關-月結存貨明細表",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=9a4cd1a78000360e4483&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=9a4cd1a78000360e4483"
     },
     {
       id: 9,
       title: "零件相關-零件銷售明細表",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=0b81eacee030bb750701&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=0b81eacee030bb750701"
     },
     {
       id: 10,
       title: "零件相關-零件收益及毛利分析",
       parent: "零件相關",
       roleList: ["售服主管", "服務廠主管", "零件主管", "零件人員"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=838a16ef9e97bcb6e1a1&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=838a16ef9e97bcb6e1a1"
     },
     {
       id: 11,
       title: "維修相關-工單維修明細表",
       parent: "維修相關",
       roleList: ["售服主管", "服務廠主管", "技術主管"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=e71ab409797476166c55&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=e71ab409797476166c55"
     },
     {
       id: 12,
       title: "維修相關-重修明細表",
       parent: "維修相關",
       roleList: ["售服主管", "服務廠主管", "技術主管"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=81692c01a7b3501a8602&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=81692c01a7b3501a8602"
     },
     {
       id: 13,
       title: "維修相關-保固明細表",
       parent: "維修相關",
       roleList: ["售服主管", "服務廠主管", "技術主管"],
-      link: "https://app.powerbi.com/reportEmbed?reportId=cbb27f790e6aad4d5abb&autoAuth=true&ctid=c0b07114-604f-4d44-ae97-335e1314b7a8",
+      link: "https://app.powerbi.com/reportEmbed?reportId=d0a56937-4a6b-4082-b021-c9a491c9ae78&autoAuth=true&ctid=81fa766e-a349-4867-8bf4-ab35e250a08f&pageName=cbb27f790e6aad4d5abb"
     },
   ]);
   return { sideMenuItems };
